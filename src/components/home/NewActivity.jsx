2da3ec8fdf841507d184e64d8682b95fa3265187
Guariko <creatingsomething2021@gmail.com>
2024-01-01 12:54:24 -0300

    Activities code adjustments

--- src/components/home/NewActivity.jsx
+++ src/components/home/NewActivity.jsx
@@ -1,4 +1,18 @@
 export default function NewActivity() {
+  const activitiesData = getActivitiesData();
+
+  const activitiesElement = activitiesData.map((activity) => {
+    return (
+      <Activity
+        key={activity.id}
+        subject={activity.subject}
+        timeSpent={activity.timeSpent}
+        datetime={activity.datetime}
+        message={activity.message}
+      />
+    );
+  });
+
   return (
     <div className="shadow-lg max-w-[30rem] border-b-8 rounded-lg border-primary">
       <div className="p-4 bg-white flex items-center gap-4">
@@ -19,132 +33,82 @@ export default function NewActivity() {
           </mark>
         </div>
 
-        <div className="overflow-auto bg-white">
-          <div className="flex flex-col gap-3 border-b border-gray-400 bg-white p-3">
-            <div className="flex justify-between gap-4">
-              <div className="flex items-center gap-3">
-                <mark className="rounded-full w-4 aspect-square bg-primary block"></mark>
-
-                <h3 className="text-base text-gray-600 font-bold">
-                  Matemática
-                </h3>
-              </div>
-
-              <div className="flex items-center gap-4">
-                <div className="flex gap-1 items-center">
-                  <i className="icon-alarm text-gray-700 text-sm"></i>
-                  <mark className="text-gray-700 font-medium text-sm">
-                    16:30
-                  </mark>
-                </div>
-
-                <div className="flex gap-1 items-center">
-                  <i className="icon-stopwatch text-gray-700 text-sm"></i>
-                  <mark className="text-gray-700 font-medium text-sm">1h</mark>
-                </div>
-              </div>
-            </div>
-
-            <p className="text-sm text-gray-500 font-medium">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere,
-              earum?
-            </p>
-          </div>
-
-          <div className="flex flex-col gap-3 border-b border-gray-400 bg-white p-3">
-            <div className="flex justify-between gap-4">
-              <div className="flex items-center gap-3">
-                <mark className="rounded-full w-4 aspect-square bg-primary block"></mark>
-
-                <h3 className="text-base text-gray-600 font-bold">
-                  Matemática
-                </h3>
-              </div>
-
-              <div className="flex items-center gap-4">
-                <div className="flex gap-1 items-center">
-                  <i className="icon-alarm text-gray-700 text-sm"></i>
-                  <mark className="text-gray-700 font-medium text-sm">
-                    16:30
-                  </mark>
-                </div>
-
-                <div className="flex gap-1 items-center">
-                  <i className="icon-stopwatch text-gray-700 text-sm"></i>
-                  <mark className="text-gray-700 font-medium text-sm">1h</mark>
-                </div>
-              </div>
-            </div>
-
-            <p className="text-sm text-gray-500 font-medium">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere,
-              earum?
-            </p>
-          </div>
-
-          <div className="flex flex-col gap-3 border-b border-gray-400 bg-white p-3">
-            <div className="flex justify-between gap-4">
-              <div className="flex items-center gap-3">
-                <mark className="rounded-full w-4 aspect-square bg-primary block"></mark>
-
-                <h3 className="text-base text-gray-600 font-bold">
-                  Matemática
-                </h3>
-              </div>
-
-              <div className="flex items-center gap-4">
-                <div className="flex gap-1 items-center">
-                  <i className="icon-alarm text-gray-700 text-sm"></i>
-                  <mark className="text-gray-700 font-medium text-sm">
-                    16:30
-                  </mark>
-                </div>
-
-                <div className="flex gap-1 items-center">
-                  <i className="icon-stopwatch text-gray-700 text-sm"></i>
-                  <mark className="text-gray-700 font-medium text-sm">1h</mark>
-                </div>
-              </div>
-            </div>
-
-            <p className="text-sm text-gray-500 font-medium">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere,
-              earum?
-            </p>
-          </div>
-
-          <div className="flex flex-col gap-3 border-b border-gray-400 bg-white p-3">
-            <div className="flex justify-between gap-4">
-              <div className="flex items-center gap-3">
-                <mark className="rounded-full w-4 aspect-square bg-primary block"></mark>
-
-                <h3 className="text-base text-gray-600 font-bold">
-                  Matemática
-                </h3>
-              </div>
-
-              <div className="flex items-center gap-4">
-                <div className="flex gap-1 items-center">
-                  <i className="icon-alarm text-gray-700 text-sm"></i>
-                  <mark className="text-gray-700 font-medium text-sm">
-                    16:30
-                  </mark>
-                </div>
-
-                <div className="flex gap-1 items-center">
-                  <i className="icon-stopwatch text-gray-700 text-sm"></i>
-                  <mark className="text-gray-700 font-medium text-sm">1h</mark>
-                </div>
-              </div>
-            </div>
-
-            <p className="text-sm text-gray-500 font-medium">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere,
-              earum?
-            </p>
-          </div>
-        </div>
+        <div className="overflow-auto bg-white">{activitiesElement}</div>
       </div>
     </div>
   );
+}
+
+function Activity(props) {
+  return (
+    <div className="flex flex-col gap-3 border-b border-gray-400 bg-white p-3">
+      <div className="flex justify-between gap-4">
+        <div className="flex items-center gap-3">
+          <mark className="rounded-full w-4 aspect-square bg-primary block"></mark>
+
+          <h3 className="text-base text-gray-600 font-bold">{props.subject}</h3>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <div className="flex gap-1 items-center">
+            <i className="icon-alarm text-gray-700 text-sm"></i>
+            <mark className="text-gray-700 font-medium text-sm">
+              {props.datetime}
+            </mark>
+          </div>
+
+          <div className="flex gap-1 items-center">
+            <i className="icon-stopwatch text-gray-700 text-sm"></i>
+            <mark className="text-gray-700 font-medium text-sm">
+              {props.timeSpent}
+            </mark>
+          </div>
+        </div>
+      </div>
+
+      <p className="text-sm text-gray-500 font-medium">{props.message}</p>
+    </div>
+  );
+}
+
+function getActivitiesData() {
+  return [
+    {
+      id: 1,
+      subject: "Matemática",
+      datetime: "16:50",
+      timeSpent: "50m",
+      message:
+        "This is my message, This is my message This is my message This is my message",
+    },
+    {
+      id: 2,
+      subject: "Português",
+      datetime: "12:50",
+      timeSpent: "50m",
+      message: "This is my message, This is my message, This is my message",
+    },
+    {
+      id: 3,
+      subject: "Inglês",
+      datetime: "11:50",
+      timeSpent: "50m",
+      message: "This is my message, This is my message",
+    },
+    {
+      id: 4,
+      subject: "Química",
+      datetime: "18:20",
+      timeSpent: "50m",
+      message: "This is my message",
+    },
+    {
+      id: 5,
+      subject: "Física",
+      datetime: "10:10",
+      timeSpent: "50m",
+      message:
+        "This is my message, This is my message This is my message This is my message",
+    },
+  ];
 }
