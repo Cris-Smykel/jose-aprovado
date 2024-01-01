4ab57c08e7550643b08b80c204e575322c9269c8
Guariko <creatingsomething2021@gmail.com>
2024-01-01 12:34:15 -0300

    Last Days code adjustments

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -4,13 +4,15 @@ import MostUsed from "./MostUsed";
 
 export default function Hero() {
   return (
-    <section className="pt-10">
-      <div className="p-4 max-w-6xl m-auto flex flex-col gap-10">
-        <LastDays />
+    <section className="pt-10 pb-10">
+      <div className="p-4 max-w-5xl m-auto flex flex-col items-center gap-10 md:flex-row md:justify-center">
+        <div className="flex flex-col gap-10">
+          <LastDays />
+
+          <MostUsed />
+        </div>
 
         <NewActivity />
-
-        <MostUsed />
       </div>
     </section>
   );
--- src/components/home/LastDays.jsx
+++ src/components/home/LastDays.jsx
@@ -1,4 +1,22 @@
 export default function LastDays() {
+  const times = getTimesData();
+
+  const timesElement = times.map((time) => {
+    return <Times time={time.time} key={time.id} />;
+  });
+
+  const staticsData = getStaticsData();
+
+  const staticElemets = staticsData.map((staticData) => {
+    return (
+      <Statics
+        height={staticData.height}
+        day={staticData.day}
+        key={staticData.id}
+      />
+    );
+  });
+
   return (
     <div className="shadow-lg max-w-[19rem] border-b-8 rounded-lg border-primary">
       <div className="p-4 bg-white flex items-center gap-2">
@@ -10,68 +28,89 @@ export default function LastDays() {
       <div className="overflow-auto bg-gray-100">
         <div className="bg-gray-100 p-4 flex gap-4">
           <div>
-            <ol className="flex flex-col gap-4">
-              <li className="flex items-center gap-2">
-                <mark className="text-xs text-gray-600 font-medium w-6">
-                  1h{" "}
-                </mark>
-                <mark>-</mark>
-              </li>
-
-              <li className="flex items-center gap-2">
-                <mark className="text-xs text-gray-600 font-medium w-6">
-                  30m
-                </mark>
-                <mark>-</mark>
-              </li>
-
-              <li className="flex items-center gap-2">
-                <mark className="text-xs text-gray-600 font-medium w-6">
-                  10m
-                </mark>
-                <mark>-</mark>
-              </li>
-            </ol>
+            <ol className="flex flex-col gap-4">{timesElement}</ol>
           </div>
 
-          <div className="flex items-end h-36 gap-2">
-            <div className="flex flex-col gap-2">
-              <div className="h-10 w-6 bg-primary"></div>
-              <mark className="text-xs text-gray-600 font-medium">Seg</mark>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <div className="h-20 w-6 bg-primary"></div>
-              <mark className="text-xs text-gray-600 font-medium">Ter</mark>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <div className="h-16 w-6 bg-primary"></div>
-              <mark className="text-xs text-gray-600 font-medium">Qua</mark>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <div className="h-28 w-6 bg-primary"></div>
-              <mark className="text-xs text-gray-600 font-medium">Qui</mark>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <div className="h-4 w-6 bg-primary"></div>
-              <mark className="text-xs text-gray-600 font-medium">Sex</mark>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <div className="h-12 w-6 bg-primary"></div>
-              <mark className="text-xs text-gray-600 font-medium">Sáb</mark>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <div className="h-10 w-6 bg-primary"></div>
-              <mark className="text-xs text-gray-600 font-medium">Hoje</mark>
-            </div>
-          </div>
+          <div className="flex items-end h-36 gap-2">{staticElemets}</div>
         </div>
       </div>
     </div>
   );
+}
+
+function Times(props) {
+  return (
+    <li className="flex items-center gap-2">
+      <mark className="text-xs text-gray-600 font-medium w-6">
+        {props.time}
+      </mark>
+      <mark>-</mark>
+    </li>
+  );
+}
+
+function getTimesData() {
+  return [
+    {
+      id: 1,
+      time: "1h",
+    },
+    {
+      id: 2,
+      time: "30m",
+    },
+    {
+      id: 3,
+      time: "10m",
+    },
+  ];
+}
+
+function Statics(props) {
+  return (
+    <div className="flex flex-col gap-2">
+      <div className={`${props.height} w-6 bg-primary`}></div>
+      <mark className="text-xs text-gray-600 font-medium">{props.day}</mark>
+    </div>
+  );
+}
+
+function getStaticsData() {
+  return [
+    {
+      id: 1,
+      height: "h-5",
+      day: "seg",
+    },
+    {
+      id: 2,
+      height: "h-10",
+      day: "ter",
+    },
+    {
+      id: 3,
+      height: "h-3",
+      day: "qua",
+    },
+    {
+      id: 4,
+      height: "h-16",
+      day: "qui",
+    },
+    {
+      id: 5,
+      height: "h-24",
+      day: "sex",
+    },
+    {
+      id: 6,
+      height: "h-6",
+      day: "sab",
+    },
+    {
+      id: 7,
+      height: "h-12",
+      day: "hoje",
+    },
+  ];
 }
