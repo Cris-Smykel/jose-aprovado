8278bd52972ec6264885aa2895803c66bda2d6b2
Guariko <creatingsomething2021@gmail.com>
2024-01-01 09:04:30 -0300

    Header set up on pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Activity from "./pages/Activity";
+import Revisions from "./pages/Revisions";
+import Planning from "./pages/Planning";
+import StudyCircle from "./pages/StudyCircle";
+import Supervision from "./pages/Supervision";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/atividades" element={<Activity />} />
+        <Route path="/revisoes" element={<Revisions />} />
+        <Route path="/planejamento" element={<Planning />} />
+        <Route path="/ciclo-de-estudos" element={<StudyCircle />} />
+        <Route path="/acompanhamento" element={<Supervision />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/StudyCircle.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+
+export default function StudyCircle() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
